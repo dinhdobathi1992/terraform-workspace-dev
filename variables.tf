4566072913c2cff8e2770dbe9d878339431e4621
Thi <67835317+dinhdobathi1992@users.noreply.github.com>
2024-04-30 21:55:01 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -29,3 +29,9 @@ variable "public_subnets" {
   type        = list(string)
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
+
+
+variable "github_token" {
+  description = "A list of private subnets inside the VPC"
+  type        = string
+}
